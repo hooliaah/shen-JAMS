1457ee0f07f6efc1e1acb32d6577fa69b44b9ca1
Alvin Go <alvingogo25@gmail.com>
2018-03-17 14:33:02 -0700

    api route template

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,49 @@
 $(document).ready(function() {
 
 
-// Scott's old testing code      
-// $("#find-me").on("click", getLocation())
+  // Scott's old testing code
+  $("#find-me").on("click", getLocation);
 
-// var x = document.getElementById("demo");
+  // var x = document.getElementById("demo");
 
-// function getLocation(){
-//    var getPosition = function (options) {
-//       return new Promise(function (resolve, reject) {
-//          navigator.geolocation.watchPosition(resolve, reject, options);
-//       });
-//       }
-      
-//       getPosition()
-//       .then((position) => {
-//          console.log(position);
-//          showPosition(position);
-//       })
-//       .catch((err) => {
-//          console.error(err.message);
-//       });
-// }
+  function getLocation() {
 
-// function showPosition(position) {
-//    var latLon = { latitude: position.coords.latitude, longitude: position.coords.longitude };
-//    x.innerHTML = "Latitude: " + latLon.latitude + 
-//    "<br>Longitude: " + latLon.longitude; 
-//    $.post("/api/locate", latLon, function(response) {
-//       console.log("sent", latLon);
-//    }
-//    ).then(function(response) {
-//       console.log("then response", response);
-//    });
-// };
-// console.log("document loaded");
+    $.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCp3aTtHLbEh8qxnsXxPGo2mJbzuRmx8QY", function(data) {
+      console.log(data)
+    }).catch((err) => {
+      console.error(err.message);
+    });
+  };
 
 });
+
+  //   var getPosition = function(options) {
+  //     return new Promise(function(resolve, reject) {
+  //       navigator.geolocation.watchPosition(resolve, reject, options);
+  //     });
+  //   }
+  //
+  //   getPosition()
+  //     .then((position) => {
+  //       console.log(position);
+  //       showPosition(position);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err.message);
+  //     });
+  // };
+  //
+  // function showPosition(position) {
+  //   var latLon = {
+  //     latitude: position.coords.latitude,
+  //     longitude: position.coords.longitude
+  //   };
+  //   x.innerHTML = "Latitude: " + latLon.latitude +
+  //     "<br>Longitude: " + latLon.longitude;
+  //   $.post("/api/locate", latLon, function(response) {
+  //     console.log("sent", latLon);
+  //   }).then(function(response) {
+  //     console.log("then response", response);
+  //   });
+  // };
+  // console.log("document loaded");
