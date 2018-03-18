29eb58552893c051aa7335fbe23005752064fc96
Julie Groth <groth.julie@gmail.com>
2018-03-18 00:29:57 -0700

    added schema, updated events model

--- models/events.js
+++ models/events.js
@@ -1,22 +1,18 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Event = sequelize.define("Event", {
-      event_name: {
-          type: DataTypes.STRING,
-          isAlpha: true,
-          notNULL: true
-      },
-      event_time: {
-          type: DataTypes.STRING,
-          notNULL: true
-      },
-      event_location: {
-          type: DataTypes.STRING,
-          notNULL: true
-      },
-      event_friends: {
-          type: DataTypes.STRING,
-          notNULL: true
-      }
+        event_name: {
+            type: DataTypes.STRING,
+            isAlpha: true,
+            notNULL: true
+        },
+        event_time: {
+            type: DataTypes.STRING,
+            notNULL: true
+        },
+        event_location: {
+            type: DataTypes.STRING,
+            notNULL: true
+        }
     });
-    return Event;
+return Event;
   };
