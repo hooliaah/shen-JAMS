332ebca54bd5f319380ec420b70067ac0e851e9f
Alvin Go <alvingogo25@gmail.com>
2018-03-18 14:39:08 -0700

    user queries

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,57 +1,58 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // post user to database
-  app.post("/api/signup/:email", function(req, res) {
+  app.post("/api/v1/signup", function(req, res) {
     db.User.create(req.body).then(function(dbPost) {
       res.json(dbPost);
+      console.log(dbPost);
     });
   });
 
+
   // get user's friends
-  app.get("/api/home/:userid/friends", function (req, res) {
+  app.get("/api/v1/home/:userid", function(req, res) {
     db.User.findAll({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbPost) {
+
+      include: [{
+        model: user_friends,
+        as: 'Friends',
+        where: {
+          UserId: req.params.userid
+        },
+        include: [Users]
+      }]
+    }).then(function(err, dbPost) {
+      if (err) {throw err};
       res.json(dbPost);
+      console.log(dbPost);
     });
   });
 
   // get user's events
-  app.get("/api/home/:userid/friends", function (req, res) {
-    db.User.findAll({}).then(function(dbPost) {
+  app.get("/api/v1/home/:userid", function(req, res) {
+    db.User.findAll({
+      // include: [{
+      //     model: user_events,
+      //     where: {
+      //       EventID: req.params.userid
+      //     },
+      //     include: [Events]
+      //   }
+      // ]
+    }).then(function(dbPost) {
       res.json(dbPost);
+      console.log(dbPost);
     });
   });
 
   // post user event
-  app.post("/api/home/:userid/events", function (req, res) {
+  app.post("/api/v1/home/:userid", function(req, res) {
     db.User.create(req.body).then(function(dbPost) {
       res.json(dbPost);
+      console.log(dbPost);
     });
   });
 
 
-}
-
-// client javascript stuff
-$(document).ready(function() {
-
-
-  // Scott's old testing code
-  $("#find-me").on("click", getLocation);
-
-  // var x = document.getElementById("demo");
-
-  function getLocation() {
-
-    $.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCp3aTtHLbEh8qxnsXxPGo2mJbzuRmx8QY", function(data) {
-      console.log(data)
-    }).catch((err) => {
-      console.error(err.message);
-    });
-  };
-
-});
+};
