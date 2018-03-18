25eae1f3774c918b3f935dfdc44fe9b44511d691
McKenna Jo Beardsley <mckennajo@McKennas-MacBook-Pro.local>
2018-03-18 13:31:26 -0700

    pages layouts update

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,41 @@
-  $(window).on(‘load’, function() {
+$(window).on('load', function() {
 
-  $("#find-me").on("click", getLocation);
 
-   $(“#login-button”).on(“click”, function () {
-       $(“#signup-button”).addClass(“convertToGrey”);
-       $(“#signup-button”).removeClass(“active”);
-       $(“#login-form”).show();
-       $(“#signup-form”).hide();
-       $(this).addClass(“active”).removeClass(“convertToGrey”);
-   });
+    $("#find-me").on("click", getLocation);
 
-   $(“#signup-button”).on(“click”, function () {
-       $(“#login-button”).addClass(“convertToGrey”);
-       $(“#login-button”).removeClass(“active”);
-       $(“#login-form”).hide();
-       $(“#signup-form”).show();
-       $(this).addClass(“active”).removeClass(“convertToGrey”);
-   });
+    //  var loginButton = $("#login-button");
+    //  var signupButton = $("#signup-button");
+    //
+    // $(".content").html(login);
 
-   $(“#signup-next”).on(“click”, function () {
 
-   })
+  $("#signup-button").addClass("convertToGrey");
 
-   function getLocation() {
 
-     $.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCp3aTtHLbEh8qxnsXxPGo2mJbzuRmx8QY", function(data) {
-       console.log(data)
-     }).catch((err) => {
-       console.error(err.message);
-     });
-   };
+  $("#login-button").on("click", function () {
+      $("#signup-button").addClass("convertToGrey");
+      $("#signup-button").removeClass("active");
+      $("#login-form").show();
+      $("#signup-form").hide();
+      $(this).addClass("active").removeClass("convertToGrey");
+  });
 
- });
+  $("#signup-button").on("click", function () {
+      $("#login-button").addClass("convertToGrey");
+      $("#login-button").removeClass("active");
+      $("#login-form").hide();
+      $("#signup-form").show();
+      $(this).addClass("active").removeClass("convertToGrey");
+  });
+
+
+  function getLocation() {
+
+    $.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCp3aTtHLbEh8qxnsXxPGo2mJbzuRmx8QY", function(data) {
+      console.log(data)
+    }).catch((err) => {
+      console.error(err.message);
+    });
+  };
+
+});
