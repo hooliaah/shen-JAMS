6e0532c51b5ae7065f722d77dcb3f2cf5095ca3c
scottdavidreynolds <scott@scottreynolds.net>
2018-03-18 12:00:20 -0700

    adding pages

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,25 @@
 $(window).on('load', function() {
 
-  $(".signup-button").addClass("convertToGrey");
+  $("#signup-button").addClass("convertToGrey");
 
-  $(".login-button").on("click", function () {
+  $("#login-button").on("click", function () {
       $("#signup-button").addClass("convertToGrey");
       $("#signup-button").removeClass("active");
+      $("#login-form").show();
+      $("#signup-form").hide();
+      $(this).addClass("active").removeClass("convertToGrey");
+  });
+
+  $("#signup-button").on("click", function () {
+      $("#login-button").addClass("convertToGrey");
+      $("#login-button").removeClass("active");
       $("#login-form").hide();
       $("#signup-form").show();
       $(this).addClass("active").removeClass("convertToGrey");
   });
 
-  $(".signup-button").on("click", function () {
-      $("#login-button").addClass("convertToGrey");
-      $("#login-button").removeClass("active");
-      $("#login-form").show();
-      $("#signup-form").hide();
-      $(this).addClass("active").removeClass("convertToGrey");
-  });
+  $("#signup-next").on("click", function () {
+     
+  })
   
 });
