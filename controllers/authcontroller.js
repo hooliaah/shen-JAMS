57553a73610a20565c3b379511da9eeae2f49ae0
Julie Groth <groth.julie@gmail.com>
2018-03-20 20:49:18 -0700

    added passport

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,13 +1,19 @@
 var exports = module.exports = {}
  
 exports.signup = function(req, res) {
-
-    res.render('signup');
- 
+    res.render('login');
 }
 
 exports.signin = function(req, res) {
- 
-    res.render('signin');
- 
+    res.render('login');
+}
+
+exports.home = function(req, res) {
+    res.render('home');
+}
+
+exports.logout = function(req, res) {
+    req.session.destroy(function(err) {
+        res.redirect('/');
+    });
 }
---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+var authController = require('../controllers/authcontroller.js');
+var passport = require('passport');
+
+module.exports = function (app) {
+    app.get('/signup', authController.signup);
+    app.get('/signin', authController.signin);
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/home',
+        failureRedirect: '/signup'
+    })),
+    app.get('/home', isLoggedIn, authController.home);
+    app.get('/logout', authController.logout);
+    app.post('/signin', passport.authenticate('local-signin', {
+        successRedirect: '/home',
+        failureRedirect: '/signin'
+    }
+    ));
+
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated())
+            return next();
+        res.redirect('/signin');
+    };
+}
